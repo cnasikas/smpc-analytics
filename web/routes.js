75b8b9051db629976e09e318012da3fdfb7d8157
Christos Nasikas <xristosnasikas@gmail.com>
2019-01-23 19:22:42 +0200

    Refactor /smpc/ routes

--- web/routes.js
+++ web/routes.js
@@ -1,16 +1,21 @@
 const express = require('express')
 const path = require('path')
 const uuidv4 = require('uuid/v4')
-const DateDiff = require('date-diff')
 
 const { db, cachedb } = require('./db')
-const { colors, importLocally, importFromServers, _writeFile, _exec, _unlinkIfExists } = require('./helpers')
+const {
+  colors,
+  importLocally,
+  importFromServers,
+  _writeFile,
+  _exec,
+  _unlinkIfExists,
+  processRequest
+} = require('./helpers')
+
+const { BASE_DIR, SIMULATION_MODE, PRINT_MSG } = require('./config')
 
 const router = express.Router()
-
-const BASE_DIR = path.resolve(__dirname, '../')
-const SIMULATION_MODE = process.argv.some((arg) => (arg === '-sim' || arg === '--sim' || arg === '-simulation' || arg === '--simulation'))
-const PRINT_MSG = (SIMULATION_MODE) ? 'NODE SIMULATION' : 'NODE'
 
 router.get('/queue', async (req, res, next) => {
   let request = req.query.request
@@ -25,165 +30,172 @@ router.get('/queue', async (req, res, next) => {
 })
 
 router.post('/count', async (req, res, next) => {
-  const content = JSON.stringify(req.body)
   const uid = uuidv4()
-  const attributes = req.body.attributes
-  const datasources = req.body.datasources
-  let cache = req.body.cache
+  let generator = `python dataset-scripts/count_main_generator.py configuration_${uid}.json`
 
-  let plot = ('plot' in req.body) // if plot exists in req.body
-  await db.put(uid, JSON.stringify({ 'status': 'running' }))
-
-  if (!plot) {
-    res.setHeader('Access-Control-Allow-Origin', '*')
-    res.status(202).json({ 'location': `/smpc/queue?request=${uid}` })
+  if (SIMULATION_MODE) {
+    generator += ' --DNS web/localDNS.json'
   }
 
-  // if filters are defined
-  let requestKey = { 'attributes': attributes, 'datasources': datasources, 'plot': plot }
-
-  if ('filters' in req.body) {
-    const filters = req.body.filters
-    // Add filter attributes for importing to list
-    for (let condition of filters.conditions) {
-      const attribute = condition.attribute
-
-      if (!attributes.includes(attribute)) {
-        attributes.push(attribute)
-      }
-    }
-
-    requestKey.plot = plot
+  let pythonScripts = {
+    generator,
+    plot: `python count_plot.py ../out_${uid}.txt ../configuration_${uid}.json`,
+    web: `python web/response.py out_${uid}.txt | python web/transform_response.py  configuration_${uid}.json --mapping mhmd-driver/mesh_mapping.json --mtrees_inverted mhmd-driver/m_inv.json`
   }
 
-  requestKey = JSON.stringify(requestKey)
-
-  // Check if request has been already computed
-  cache = cache && cache.toUpperCase() !== 'NO'
-
-  let computationResponse = ''
-  let cachedResponse = null
-
-  if (cache) {
-    cachedResponse = await getFromCache(requestKey, uid, plot)
+  let importOptions = {
+    uri: '/smpc/import',
+    type: 'mesh',
+    dnsFile: 'MHMDdns.json'
   }
 
   try {
-    if (cache && cachedResponse) {
-      if (plot) {
-        return res.status(200).json(JSON.parse(cachedResponse.slice(1, -1))) // slice is for removing quotes from string.
-      } else {
-        console.log('[' + PRINT_MSG + '] Request(' + uid + ') Response ready.\n')
-        const resultObj = { 'status': 'succeeded', 'result': '' }
-        resultObj.result = JSON.parse(cachedResponse)
-        await db.put(uid, JSON.stringify(resultObj))
-      }
-    } else {
-      computationResponse = await computeCount(attributes, datasources, uid, content, plot)
-
-      let toCache = ''
-      let graphName = ''
-
-      if (plot) {
-        console.log('[' + PRINT_MSG + '] Request(' + uid + ') Plotting done.\n')
-        graphName = computationResponse.toString()
-        graphName = graphName.slice(0, -1)
-        console.log('[' + PRINT_MSG + '] Request(' + uid + ') ' + graphName)
-        toCache = JSON.stringify(computationResponse.replace(/\n/g, ''))
-      } else {
-        console.log('[' + PRINT_MSG + '] Request(' + uid + ') Response ready.\n')
-        const resultObj = { 'status': 'succeeded', 'result': '' }
-        resultObj.result = JSON.parse(computationResponse)
-        await db.put(uid, JSON.stringify(resultObj))
-        toCache = computationResponse
-      }
-
-      await cachedb.put(requestKey, toCache + ', date:' + new Date())
-
-      if (plot) {
-        return res.send(graphName)
-      }
-    }
+    await computeHistogram(req, res, uid, pythonScripts, importOptions)
   } catch (err) {
     console.log(colors.red + '[' + PRINT_MSG + '] ' + colors.reset + err)
-    return next(err)
+    next(err)
   }
 })
 
-const computeCount = async (attributes, datasources, uid, content, plot = false) => {
+router.post('/histogram', async (req, res, next) => {
+  const uid = uuidv4()
+
+  let generator = `python dataset-scripts/main_generator.py configuration_${uid}.json --DNS web/`
+  generator = SIMULATION_MODE ? generator + 'localDNS.json' : generator + 'MHMDdns_cvi.json'
+
+  let pythonScripts = {
+    generator,
+    plot: `python plot.py ../configuration_${uid}.json`,
+    web: `python web/response.py out_${uid}.txt`
+  }
+
+  let importOptions = {
+    uri: '/smpc/import/cvi',
+    type: 'cvi',
+    dnsFile: 'MHMDdns_cvi.json'
+  }
+
+  try {
+    await computeHistogram(req, res, uid, pythonScripts, importOptions)
+  } catch (err) {
+    console.log(colors.red + '[' + PRINT_MSG + '] ' + colors.reset + err)
+    next(err)
+  }
+})
+
+const computeHistogram = async (req, res, uid, pythonScripts, importOptions) => {
+  let { attributes, datasources, content, cache, plot, cachedResponse, requestKey } = processRequest(uid, req, res)
+  let computationResponse = ''
+
+  if (cache && cachedResponse) {
+    if (plot) {
+      return res.status(200).json(JSON.parse(cachedResponse.slice(1, -1))) // slice is for removing quotes from string.
+    } else {
+      console.log('[' + PRINT_MSG + '] Request(' + uid + ') Response ready.\n')
+      const resultObj = { 'status': 'succeeded', 'result': '' }
+      resultObj.result = JSON.parse(cachedResponse)
+      await db.put(uid, JSON.stringify(resultObj))
+    }
+  } else {
+    console.log(`[${PRINT_MSG}]${colors.yellow} Request(${uid}) Key: ${requestKey} not found in cache-db.\n${colors.reset}`)
+    computationResponse = await smpcHistogram({ attributes, datasources, uid, content, plot }, pythonScripts, importOptions)
+
+    let toCache = ''
+    let graphName = ''
+
+    if (plot) {
+      console.log('[' + PRINT_MSG + '] Request(' + uid + ') Plotting done.\n')
+      graphName = computationResponse.toString()
+      graphName = graphName.slice(0, -1)
+      console.log('[' + PRINT_MSG + '] Request(' + uid + ') ' + graphName)
+      toCache = JSON.stringify(computationResponse.replace(/\n/g, ''))
+    } else {
+      console.log('[' + PRINT_MSG + '] Request(' + uid + ') Response ready.\n')
+      const resultObj = { 'status': 'succeeded', 'result': '' }
+      resultObj.result = JSON.parse(computationResponse)
+      await db.put(uid, JSON.stringify(resultObj))
+      toCache = computationResponse
+    }
+
+    await cachedb.put(requestKey, toCache + ', date:' + new Date())
+
+    if (plot) {
+      return res.send(graphName)
+    }
+  }
+}
+
+const smpcHistogram = async (request, pythonScripts, importOptions) => {
   let importPromises = []
 
   if (SIMULATION_MODE) {
-    importPromises = importLocally(attributes, datasources, BASE_DIR, uid, 'mesh')
+    importPromises = await importLocally(request.attributes, request.datasources, request.uid, importOptions.type)
   } else {
-    importPromises = importFromServers(attributes, datasources, BASE_DIR, uid, '/smpc/import', 'MHMDdns.json')
+    importPromises = await importFromServers(request.attributes, request.datasources, request.uid, importOptions.uri, importOptions.dnsFile)
   }
 
   await Promise.all(importPromises)
   console.log(colors.green + 'Importing Finished ' + colors.reset)
 
-  await _writeFile(path.join(BASE_DIR, `configuration_${uid}.json`), content, 'utf8')
-  console.log('[' + PRINT_MSG + '] Request(' + uid + ') Configuration file was saved.\n')
+  await _writeFile(path.join(BASE_DIR, `configuration_${request.uid}.json`), request.content, 'utf8')
+  console.log('[' + PRINT_MSG + '] Request(' + request.uid + ') Configuration file was saved.\n')
 
-  let cmd = `python dataset-scripts/main_generator.py configuration_${uid}.json --DNS web/`
-  cmd = SIMULATION_MODE ? cmd + 'localDNS.json' : cmd + 'MHMDdns_cvi.json'
-
-  await _exec(cmd, {
+  await _exec(pythonScripts.generator, {
     stdio: [0, 1, 2],
     cwd: BASE_DIR,
     shell: '/bin/bash'
   })
 
-  console.log('[' + PRINT_MSG + '] Request(' + uid + ') Main generated.\n')
+  console.log('[' + PRINT_MSG + '] Request(' + request.uid + ') Main generated.\n')
   const dbMsg = SIMULATION_MODE ? 'SecreC code generated. Now compiling.' : 'SecreC code generated. Now compiling and running.'
 
-  await db.put(uid, JSON.stringify({ 'status': 'running', 'step': dbMsg }))
-  await _unlinkIfExists(path.join(BASE_DIR, `/histogram/.main_${uid}.sb.src`))
-  console.log('[' + PRINT_MSG + '] Old .main_' + uid + '.sb.src deleted.\n')
+  await db.put(request.uid, JSON.stringify({ 'status': 'running', 'step': dbMsg }))
+  await _unlinkIfExists(path.join(BASE_DIR, `/histogram/.main_${request.uid}.sb.src`))
+  console.log('[' + PRINT_MSG + '] Old .main_' + request.uid + '.sb.src deleted.\n')
 
   const execArg = (SIMULATION_MODE) ? 'sharemind-scripts/compile.sh histogram/main_' : 'sharemind-scripts/sm_compile_and_run.sh histogram/main_'
-  await _exec(`${execArg}${uid}.sc`, { stdio: [0, 1, 2], cwd: BASE_DIR, shell: '/bin/bash' })
+  await _exec(`${execArg}${request.uid}.sc`, { stdio: [0, 1, 2], cwd: BASE_DIR, shell: '/bin/bash' })
 
-  await db.put(uid, JSON.stringify({
+  await db.put(request.uid, JSON.stringify({
     'status': 'running',
     'step': 'SecreC code compiled. Now running.'
   }))
 
-  console.log('[NODE SIMULATION] Request(' + uid + ') Program compiled.\n')
+  console.log('[NODE SIMULATION] Request(' + request.uid + ') Program compiled.\n')
 
-  await _exec('sharemind-scripts/run.sh histogram/main_' + uid + '.sb 2> out_' + uid + '.txt', {
+  await _exec('sharemind-scripts/run.sh histogram/main_' + request.uid + '.sb 2> out_' + request.uid + '.txt', {
     stdio: [0, 1, 2],
     cwd: BASE_DIR,
     shell: '/bin/bash'
   })
 
-  await db.put(uid, JSON.stringify({
+  await db.put(request.uid, JSON.stringify({
     'status': 'running',
     'step': 'SecreC code compiled and run. Now generating output.'
   }))
 
-  console.log('[NODE] Request(' + uid + ') Program executed.\n')
+  console.log('[NODE] Request(' + request.uid + ') Program executed.\n')
 
-  await _exec('grep --fixed-strings --text "`grep --text "' + uid + '" /etc/sharemind/server.log | tail -n 1 | cut -d " "  -f "7-8"`" /etc/sharemind/server.log | cut -d " "  -f "9-" >  out_' + uid + '.txt', {
+  await _exec('grep --fixed-strings --text "`grep --text "' + request.uid + '" /etc/sharemind/server.log | tail -n 1 | cut -d " "  -f "7-8"`" /etc/sharemind/server.log | cut -d " "  -f "9-" >  out_' + request.uid + '.txt', {
     stdio: [0, 1, 2],
     cwd: BASE_DIR,
     shell: '/bin/bash'
   })
 
   if (SIMULATION_MODE) {
-    await db.put(uid, JSON.stringify({
+    await db.put(request.uid, JSON.stringify({
       'status': 'running',
       'step': 'SecreC code run. Now generating output.'
     }))
-    console.log('[NODE SIMULATION] Request(' + uid + ') Program executed.\n')
+    console.log('[NODE SIMULATION] Request(' + request.uid + ') Program executed.\n')
   }
 
   let execResults = ''
 
-  if (plot) {
-    execResults = await _exec('python plot.py ../configuration_' + uid + '.json')
+  if (request.plot) {
+    execResults = await _exec(pythonScripts.plot)
   } else {
-    execResults = await _exec('python web/response.py out_' + uid + '.txt', { cwd: BASE_DIR, shell: '/bin/bash' })
+    execResults = await _exec(pythonScripts.web, { cwd: BASE_DIR, shell: '/bin/bash' })
   }
 
   const { stdout, stderr } = execResults
@@ -193,24 +205,6 @@ const computeCount = async (attributes, datasources, uid, content, plot = false)
   }
 
   return stdout
-}
-
-const getFromCache = async (requestKey, uid) => {
-  let value = await cachedb.get(requestKey)
-  console.log('[' + PRINT_MSG + '] ' + colors.green + 'Request(' + uid + ') Key: ' + requestKey + ' found in cache-db!\n' + colors.reset)
-
-  let valueArray = value.split(', date:')
-  let diff = new DateDiff(new Date(), new Date(valueArray[1]))
-
-  // if previous computation was a month ago, recompute it
-  if (diff.days() > 30) {
-    console.log('[' + PRINT_MSG + '] ' + colors.yellow + 'Request(' + uid + ') Key: ' + requestKey + ' has expired, goind to recompute it!\n' + colors.reset)
-    await cachedb.del(requestKey)
-
-    return null
-  }
-
-  return valueArray[0]
 }
 
 module.exports = {
